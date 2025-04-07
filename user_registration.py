1abe29ef3ccfcebbba5c8d3bb5bd30cb1920f5cf
Aleena <saramathewsaleena@gmail.com>
2025-04-07 23:44:35 +0530

    check valid emailID

--- user_registration.py
+++ user_registration.py
@@ -1,17 +1,31 @@
 import re
 
-is_first_name_valid = None
-while is_first_name_valid == None:
+def check_name(name):
+    """
+    This function checks if the given name is starting with a capital letter and has a minimum of 3 letters
+    """
+    return re.fullmatch(r"[A-Z][a-z]{2,}", name)
+
+def check_email(email):
+    """
+    This function checks if the given mail id is valid or not
+    """
+    return re.fullmatch(r"^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$",email)
+
+
+first_name = input("\nEnter First Name: ")
+while check_name(first_name) is None:
+    print("\nName should start with a capital letter and have at least 3 letters!")
     first_name = input("\nEnter First Name: ")
-    is_first_name_valid = re.fullmatch(r"\b[A-Z][a-z]{2,}",first_name)
-    if is_first_name_valid == None:
-            print("Name should start with capital letter and have atleast 3 letters!!")
 
+last_name = input("\nEnter Last Name: ")
+while check_name(last_name) is None:
+    print("\nName should start with a capital letter and have at least 3 letters!")
+    first_name = input("\nEnter Last Name: ")
+    
 
-is_last_name_valid = None
-while is_last_name_valid == None:
-    last_name = input("\nEnter Last Name: ")
-    is_last_name_valid = re.fullmatch(r"\b[A-Z][a-z]{2,}",last_name)
-    if is_last_name_valid == None:
-            print("Name should start with capital letter and have atleast 3 letters!!")
+email_id = input("\nEnter your EmailID: ")
+while check_email(email_id) is None:
+    print("\nEnter Valid EmailID!")
+    first_name = input("\nEnter your EmailID: ")
     
