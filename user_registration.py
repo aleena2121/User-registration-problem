d557f2799e124563b3bf902788fdc3a84510cdea
Aleena <saramathewsaleena@gmail.com>
2025-04-07 16:43:34 +0530

    check valid last name

--- user_registration.py
+++ user_registration.py
@@ -1,9 +1,17 @@
 import re
 
-first_name_check = None
-while first_name_check == None:
+is_first_name_valid = None
+while is_first_name_valid == None:
     first_name = input("\nEnter First Name: ")
-    first_name_check = re.fullmatch(r"\b[A-Z][a-z]{2,}",first_name)
-    if first_name_check == None:
+    is_first_name_valid = re.fullmatch(r"\b[A-Z][a-z]{2,}",first_name)
+    if is_first_name_valid == None:
+            print("Name should start with capital letter and have atleast 3 letters!!")
+
+
+is_last_name_valid = None
+while is_last_name_valid == None:
+    last_name = input("\nEnter Last Name: ")
+    is_last_name_valid = re.fullmatch(r"\b[A-Z][a-z]{2,}",last_name)
+    if is_last_name_valid == None:
             print("Name should start with capital letter and have atleast 3 letters!!")
     
