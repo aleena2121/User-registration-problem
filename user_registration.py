ad549242951395b2282a0f92d1103c3dee50ef5d
Aleena <saramathewsaleena@gmail.com>
2025-04-07 23:52:01 +0530

    check valid phone number

--- user_registration.py
+++ user_registration.py
@@ -12,6 +12,11 @@ def check_email(email):
     """
     return re.fullmatch(r"^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$",email)
 
+def check_phone_number(number):
+    """
+    This function checks if the given number follows this format :- Country code follow by space and 10 digit number
+    """
+    return re.fullmatch(r"\d{2}\s\d{10}",number)
 
 first_name = input("\nEnter First Name: ")
 while check_name(first_name) is None:
@@ -22,10 +27,14 @@ last_name = input("\nEnter Last Name: ")
 while check_name(last_name) is None:
     print("\nName should start with a capital letter and have at least 3 letters!")
     first_name = input("\nEnter Last Name: ")
-    
 
 email_id = input("\nEnter your EmailID: ")
 while check_email(email_id) is None:
     print("\nEnter Valid EmailID!")
     first_name = input("\nEnter your EmailID: ")
+
+phone_number = input("\nEnter your phone number: ")
+while check_phone_number(phone_number) is None:
+    print("\nEnter Valid phone number!!")
+    phone_number = input("\nEnter your phone number: ")
     
